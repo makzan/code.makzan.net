e504cb1d227f7f1b7b91a0c8bfa37bf589de2503
Thomas Seng Hin Mak <mak@makzan.net>
2022-01-19 16:54:29 +0800

    Add gitRepoContentPath

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
       resolve: 'gatsby-theme-code-notes',
       options: {
         contentPath: 'notes',
+        gitRepoContentPath: 'https://github.com/makzan/code.makzan.net/tree/master/notes/',
         basePath: '/',
         showThemeInfo: false,
         showDescriptionInSidebar: true,
